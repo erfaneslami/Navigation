c36ecdc654e01a95924eeb79224c9db7587ee3c0
erfan eslami <erfan.88.eslami@gmail.com>
2022-04-29 15:34:01 +0430

    add nav variant transition

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -2,14 +2,20 @@ import { filterProps, motion } from "framer-motion";
 import Classes from "./Nav.module.scss";
 
 const navVariants = {
-  show: {
+  show: (i) => ({
     opacity: 1,
     scale: 1,
-  },
-  exit: {
+    transition: {
+      delay: 0.36 - i * 0.03,
+    },
+  }),
+  exit: (i) => ({
     opacity: 0,
     scale: 0.8,
-  },
+    transition: {
+      delay: i * 0.03,
+    },
+  }),
 };
 
 const Nav = (props) => {
@@ -19,108 +25,131 @@ const Nav = (props) => {
         <ul className={Classes.desktopNav}>
           <motion.li
             variants={navVariants}
+            custom={0}
             initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0 }}
+            // transition={{ delay: 0 }}
           >
             <a href="" className={Classes.logo}></a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={1}
             initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.03 }}
+            // transition={{ delay: 0.03 }}
           >
             <a href="">فروشگاه</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={2}
             initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.06 }}
+            // transition={{ delay: 0.06 }}
           >
             <a href="">مک</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={3}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.09 }}
+            // transition={{ delay: 0.09 }}
           >
             <a href="">آیپد</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={4}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.12 }}
+            // transition={{ delay: 0.12 }}
           >
             <a href="">آیفون</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={5}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.15 }}
+            // transition={{ delay: 0.15 }}
           >
             <a href="">اپل واچ</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={6}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.18 }}
+            // transition={{ delay: 0.18 }}
           >
             <a href="">ایرپاد</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={7}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.21 }}
+            // transition={{ delay: 0.21 }}
           >
             <a href="">اپل هوم</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={8}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.25 }}
+            // transition={{ delay: 0.25 }}
           >
             <a href="">لوازم جانبی</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={9}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.28 }}
+            // transition={{ delay: 0.28 }}
           >
             <a href="">فقط در اپل</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={10}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.31 }}
+            // transition={{ delay: 0.31 }}
           >
             <a href="">پشتیبانی</a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={11}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.34 }}
+            // transition={{ delay: 0.34 }}
           >
             <a href="#" className={Classes.search} onClick={props.onSearch}></a>
           </motion.li>
           <motion.li
             variants={navVariants}
+            custom={12}
+            initial="exit"
             animate="show"
             exit="exit"
-            transition={{ delay: 0.37 }}
+            // transition={{ delay: 0.37 }}
           >
             <a href="" className={Classes.cart}></a>
           </motion.li>
