8945fbc3cdded3a1c017f72f41d3371eb5f92088
erfan eslami <erfan.88.eslami@gmail.com>
2022-04-29 10:45:53 +0430

    add framer motion

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Classes from "./Header.module.scss";
 import Nav from "./Nav";
 import Search from "./Search";
 
 const Header = () => {
+  const [isSearching, setIsSearching] = useState(false);
+
+  const hanlder = () => {
+    setIsSearching(true);
+  };
+
   return (
     <>
       <div className={Classes["nav-container"]}>
-        {/* <Nav /> */}
+        {!isSearching && <Nav />}
         <Search />
+        <button onClick={hanlder}>click</button>
       </div>
     </>
   );
--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -1,54 +1,53 @@
+import { motion } from "framer-motion";
 import Classes from "./Nav.module.scss";
 
 const Nav = () => {
   return (
-    <>
-      <div className={Classes["nav-container"]}>
-        <nav>
-          <ul className={Classes.desktopNav}>
-            <li>
-              <a href="" className={Classes.logo}></a>
-            </li>
-            <li>
-              <a href="">فروشگاه</a>
-            </li>
-            <li>
-              <a href="">مک</a>
-            </li>
-            <li>
-              <a href="">آیپد</a>
-            </li>
-            <li>
-              <a href="">آیفون</a>
-            </li>
-            <li>
-              <a href="">اپل واچ</a>
-            </li>
-            <li>
-              <a href="">ایرپاد</a>
-            </li>
-            <li>
-              <a href="">اپل هوم</a>
-            </li>
-            <li>
-              <a href="">لوازم جانبی</a>
-            </li>
-            <li>
-              <a href="">فقط در اپل</a>
-            </li>
-            <li>
-              <a href="">پشتیبانی</a>
-            </li>
-            <li>
-              <a href="" className={Classes.search}></a>
-            </li>
-            <li>
-              <a href="" className={Classes.cart}></a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </>
+    <motion.div exit={{ scale: 0.5 }} className={Classes["nav-container"]}>
+      <nav>
+        <ul className={Classes.desktopNav}>
+          <li>
+            <a href="" className={Classes.logo}></a>
+          </li>
+          <li>
+            <a href="">فروشگاه</a>
+          </li>
+          <li>
+            <a href="">مک</a>
+          </li>
+          <li>
+            <a href="">آیپد</a>
+          </li>
+          <li>
+            <a href="">آیفون</a>
+          </li>
+          <li>
+            <a href="">اپل واچ</a>
+          </li>
+          <li>
+            <a href="">ایرپاد</a>
+          </li>
+          <li>
+            <a href="">اپل هوم</a>
+          </li>
+          <li>
+            <a href="">لوازم جانبی</a>
+          </li>
+          <li>
+            <a href="">فقط در اپل</a>
+          </li>
+          <li>
+            <a href="">پشتیبانی</a>
+          </li>
+          <li>
+            <a href="" className={Classes.search}></a>
+          </li>
+          <li>
+            <a href="" className={Classes.cart}></a>
+          </li>
+        </ul>
+      </nav>
+    </motion.div>
   );
 };
 
