4e145fefc21b22ccffb5fd96f998a5e341fb2fc0
erfan eslami <erfan.88.eslami@gmail.com>
2022-04-30 19:14:14 +0430

    add mobile nav search

--- src/Components/Header/MobileNav.js
+++ src/Components/Header/MobileNav.js
@@ -5,14 +5,33 @@ import Classes from "./MobileNav.module.scss";
 const menuVariant = {
   initial: {
     height: 0,
+
     transition: {
       duration: 0.4,
     },
   },
   animate: {
     height: "100vh",
+
+    // height: "100%",
     transition: {
       duration: 0.4,
+    },
+  },
+};
+
+const searchMenuVariant = {
+  initial: {
+    height: 0,
+    transition: {
+      type: "tween",
+      delay: 0.02,
+    },
+  },
+  animate: {
+    height: "auto",
+    transition: {
+      type: "tween",
     },
   },
 };
@@ -49,44 +68,76 @@ const MobileNav = (props) => {
 
       <AnimatePresence>
         {isOpenMenu && (
-          <motion.ul
+          <motion.div
             variants={menuVariant}
             initial="initial"
             animate="animate"
             exit="initial"
-            className={Classes.mobileNavList}
+            className={Classes.menu}
           >
-            <li>
-              <a href="#">فروشگاه</a>
-            </li>
-            <li>
-              <a href="#">مک</a>
-            </li>
-            <li>
-              <a href="#">آیپد</a>
-            </li>
-            <li>
-              <a href="#">آیفون</a>
-            </li>
-            <li>
-              <a href="#">اپل واچ</a>
-            </li>
-            <li>
-              <a href="#">ایرپاد</a>
-            </li>
-            <li>
-              <a href="#">اپل هوم</a>
-            </li>
-            <li>
-              <a href="#">لوازم جانبی</a>
-            </li>
-            <li>
-              <a href="#">فقط در اپل</a>
-            </li>
-            <li>
-              <a href="#">پشتیبانی</a>
-            </li>
-          </motion.ul>
+            <div className={Classes.searchContainer}>
+              <motion.form
+                variants={searchMenuVariant}
+                initial="initial"
+                animate="animate"
+                exit="initial"
+              >
+                <motion.button
+                  initial={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  type="button"
+                  className={Classes.cancelBtn}
+                >
+                  کنسل
+                </motion.button>
+                <motion.input
+                  // initial={{ padding: " 1.2rem 2.4rem" }}
+                  // exit={{ padding: 0 }}
+                  initial={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  type="text"
+                  placeholder=" جستجوی محصولات ..."
+                />
+                <motion.div
+                  initial={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className={Classes.searchLogo}
+                ></motion.div>
+              </motion.form>
+            </div>
+            <motion.ul variants={menuVariant} className={Classes.mobileNavList}>
+              <li>
+                <a href="#">فروشگاه</a>
+              </li>
+              <li>
+                <a href="#">مک</a>
+              </li>
+              <li>
+                <a href="#">آیپد</a>
+              </li>
+              <li>
+                <a href="#">آیفون</a>
+              </li>
+              <li>
+                <a href="#">اپل واچ</a>
+              </li>
+              <li>
+                <a href="#">ایرپاد</a>
+              </li>
+              <li>
+                <a href="#">اپل هوم</a>
+              </li>
+              <li>
+                <a href="#">لوازم جانبی</a>
+              </li>
+              <li>
+                <a href="#">فقط در اپل</a>
+              </li>
+              <li>
+                <a href="#">پشتیبانی</a>
+              </li>
+            </motion.ul>
+          </motion.div>
         )}
       </AnimatePresence>
     </>
